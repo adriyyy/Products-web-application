7820c25f7ceafde36053e13a3dadaa743ab561b0
Adriana <goiua4493@gmail.com>
2024-11-03 22:08:16 +0200

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+app.use(express.json());
 
 app.use("/api/products", router);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "frontend", "dist", "index.html");
+  });
+}
+
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
